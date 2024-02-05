7b2e65ed8b88b22d33e10ebfa1a6bcb008118023
JamesSand <shazz20@mails.tsinghua.edu.cn>
2024-02-05 05:56:07 +0000

    v75

--- scripts/train_5.sh
+++ scripts/train_5.sh
@@ -4,7 +4,7 @@ export CUDA_VISIBLE_DEVICES=5
 export LD_LIBRARY_PATH=/home/zhsha/miniconda3/envs/pbrs/lib
 
 # save train script
-WAND_NAME="h1_v63"
+WAND_NAME="h1_v74"
 CONFIG_BACKUP_DIR="/mnt/hypercube/zhsha/workspace/pbrs-humanoid/config_bakcup/${WAND_NAME}"
 mkdir -p $CONFIG_BACKUP_DIR
 script_path="$(readlink -f "$0")"
@@ -15,7 +15,7 @@ python gpugym/scripts/train.py \
     --headless \
     --wandb_project "pbrs3" \
     --wandb_name $WAND_NAME \
-    --h1_urdf_version 8 \
+        --h1_urdf_version 8 \
     --action_scale 0.5 \
     --ori_term_threshold 1.0 \
     --ankle_stiffness 60.0 \
@@ -27,5 +27,7 @@ python gpugym/scripts/train.py \
     --lin_vel_x_max 4.5 \
     --lin_vel_y_ab 0.75 \
     --ang_vel_yaw_abs 2.0 \
-    --fixed_range 1
+    --knee_range_low 1.4 \
+    --knee_range_high 1.5 
+
 
